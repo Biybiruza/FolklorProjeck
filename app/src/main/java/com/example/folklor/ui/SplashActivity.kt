9fbd5ac0877431309c082bfc3d97b3da850c52b9
Пользователь <Biybiruza>
2021-12-05 19:12:33 +0500

    favorite

--- app/src/main/java/com/example/folklor/ui/SplashActivity.kt
+++ app/src/main/java/com/example/folklor/ui/SplashActivity.kt
@@ -1,11 +1,29 @@
 package com.example.folklor.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.WindowManager
+import com.example.folklor.MainActivity
+import com.example.folklor.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000) // 5000 is the delayed time in milliseconds.
     }
 }
