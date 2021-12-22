409d5f3749f117d7f4835b6a9e05a36c48922084
Пользователь <Biybiruza>
2021-12-22 15:34:48 +0500

    added

--- app/src/main/java/com/example/folklor/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/folklor/ui/home/HomeFragment.kt
@@ -2,17 +2,11 @@ package com.example.folklor.ui.home
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.example.folklor.R
 import com.example.folklor.ui.MarginItemDecoration
-import com.github.barteksc.pdfviewer.PDFView
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
@@ -21,7 +15,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        recyclerView.addItemDecoration(MarginItemDecoration(8,128))
+        recyclerView.addItemDecoration(MarginItemDecoration(6,16))
         recyclerView.adapter = adapter
         adapter.setOnClickItemListener {
             val intent = Intent(requireContext(),PDFReaderView::class.java)
