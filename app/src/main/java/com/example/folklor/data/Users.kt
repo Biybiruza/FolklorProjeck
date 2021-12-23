bf8a0bf93560e83b61c72e9e30e027806a8f6005
Пользователь <Biybiruza>
2021-12-23 19:39:27 +0500

    realtime data read

---
+++ app/src/main/java/com/example/folklor/data/Users.kt
@@ -0,0 +1,3 @@
+package com.example.folklor.data
+
+data class Users()
---
+++ app/src/main/java/com/example/folklor/ui/slideshow/UserListAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.folklor.ui.slideshow
+
+import androidx.fragment.app.Fragment
+import com.example.folklor.R
+
+class FirebaseListAdapter:  {
+}
